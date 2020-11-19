81b6ae4b9842c7a74f12681b6e69bc2ad7700a77
CR <931009686@qq.com>
2020-11-20 02:45:00 +0800

    部署服务器时区调整

--- src/main/java/com/cr/toutiao/ToutiaoApplication.java
+++ src/main/java/com/cr/toutiao/ToutiaoApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 /**
  * @author cr
  */
@@ -12,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToutiaoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 		SpringApplication.run(ToutiaoApplication.class, args);
 	}
 
